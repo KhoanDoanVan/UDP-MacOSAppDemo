c851bc6c01c25d541d71a9eb4e10e1a3c2f6efb4
khoanisdev <doanvankhoan124@gmail.com>
2025-02-10 21:22:30 +0700

    Initial Commit

---
+++ UDP-MacOSApp/UDP_MacOSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UDP_MacOSAppApp.swift
+//  UDP-MacOSApp
+//
+//  Created by Đoàn Văn Khoan on 10/2/25.
+//
+
+import SwiftUI
+
+@main
+struct UDP_MacOSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
